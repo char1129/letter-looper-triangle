c8c30fb4e3fcf19ef0fcea90a13adac59c1a37e9
Sarah Charity <55107755+char1129@users.noreply.github.com>
2020-03-03 10:12:02 -0500

    Incease loops and fix spacing

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var letter = prompt('Enter a letter?');
 var i;
 var j;
 
-for (i = 0; i < 10; i++) {
-  for(j = 0; j < i; j++){
+for (i = 0; i < 11; i++) {
+  for (j = 0; j < i; j++) {
     document.write(letter);
   }
   document.write('<br>');
